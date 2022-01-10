2604253f4c0b907dbf870f6cfa58a8ae68e3826c
Mvl-Balaji <balaji.narasimhan@fourkites.com>
2022-01-10 22:29:22 +0530

    webpage

---
+++ page-2/script.js
@@ -0,0 +1,28 @@
+// let a = document.createElement("div");
+// a.innerHTML = "text";
+// document.body.append(a);
+// // document.getElementsByTagName("p").innerText = "ewdwe";
+// // console.log((document.getElementById("id").innerText = "Balaji"));
+// document.getElementsByClassName("class")[0].innerHTML = "<span>Tdse</span>";
+// // console.log(document.getElementsByClassName("class")[0].innerText);
+
+// let temp = document.createElement("b");
+// temp.innerHTML = "Hello world";
+// document.getElementById("id").innerHTML = JSON.stringify(temp);
+
+// function add() {
+//   let add = document.getElementById("country-value").value;
+//   let ans = document.createElement("option");
+//   ans.text = add;
+
+//   let flag = 0;
+//   for (let i = 0; i < document.getElementsByTagName("option").length; i++) {
+//     document.getElementsByTagName("option")[i].innerText === add
+//       ? flag++
+//       : flag;
+//   }
+
+//   flag === 0 ? document.getElementById("country").append(ans) : null;
+//   console.log(add);
+//   add = "";
+// }
